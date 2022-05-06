470af1b94fd34b80cf015384d7bb4fbf06dd3f77
Luiz Tavares <81531056+LuizTavares-SP@users.noreply.github.com>
2022-05-06 00:27:24 -0300

    Padrão MVC

--- src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
+++ src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
@@ -2,15 +2,41 @@ package com.dio.santander.bankline.api.model;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tab_movimentacao")
 public class Movimentacao {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@Column(name = "data_hora")
 	private LocalDateTime datahora;
 	private String descricao;
 	private Double valor;
+	
+	@Enumerated(EnumType.STRING)
 	private MovimentacaoTipo tipo;
 	
+	@Column(name = "id_conta")
+	private Integer idConta;
 	
+	
+	public Integer getIdConta() {
+		return idConta;
+	}
+	public void setIdConta(Integer idConta) {
+		this.idConta = idConta;
+	}
 	public Integer getId() {
 		return id;
 	}
